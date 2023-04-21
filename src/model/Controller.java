bb4c31796689b89735d8933df6f18d6242b3d406
Sebastian._.411._ <84807030+Debug41@users.noreply.github.com>
2023-04-21 08:35:42 -0500

    Se ha implementado el registro de productos, y la persistencia del programa, NOTA: aun falta implementar los metodos saveOrder y saveProduct al momento de registrar

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -1,11 +1,104 @@
 package model;
+import com.google.gson.Gson;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Controller
  */
 public class Controller {
+    private ArrayList<Order> orders = new ArrayList<>();
+    private ArrayList<Product> products = new ArrayList<>();
+    private String folder = "data";
+    private String order = "data/order.txt";
+    private String product = "data/product.txt";
 
+    public Controller() {
+    }
+
+    public boolean registerProduct(String name, String description, double price, int availableAmount, int category, int purchasedTimes){
+        Product p = new Product(name, description, price, availableAmount, Category.values()[category], purchasedTimes);
+        return products.add(p);
+    }
+
+    public String showCategory(){
+        String txt = "";
+        for (int i = 0; i < Category.values().length; i++){
+            txt = txt  + (i+1) + ". " + Category.values()[i];
+        }
+        return txt;
+    }
+
+    public void loadOrder() throws IOException {
+        File file = new File(order);
+        if (file.exists()) {
+            FileInputStream fis = new FileInputStream(file);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+            String content = "";
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                content += line + "\n";
+            }
+            Gson gson = new Gson();
+            Order[] array = gson.fromJson(content, Order[].class);
+            Collections.addAll(orders, array);
+            fis.close();
+        }else{
+            File f = new File(folder);
+            if(!f.exists()){
+                f.mkdirs();
+            }
+            file.createNewFile();
+        }
+    }
+
+    public void loadProduct() throws IOException {
+        File file = new File(product);
+        if (file.exists()) {
+            FileInputStream fis = new FileInputStream(file);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+            String content = "";
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                content += line + "\n";
+            }
+            Gson gson = new Gson();
+            Product[] array = gson.fromJson(content, Product[].class);
+            Collections.addAll(products, array);
+            fis.close();
+        }else{
+            File f = new File(folder);
+            if(!f.exists()){
+                f.mkdirs();
+            }
+            file.createNewFile();
+        }
+    }
+
+    public void saveOrder() throws IOException {
+        File file = new File(order);
+        FileOutputStream fos = new FileOutputStream(file);
+
+        Gson gson = new Gson();
+        String data = gson.toJson(orders);
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fos));
+        writer.write(data);
+        writer.flush();
+        fos.close();
+
+    }
+    public void saveProduct() throws IOException {
+        File file1 = new File(product);
+        FileOutputStream fos1 = new FileOutputStream(file1);
+        Gson gson1 = new Gson();
+        String data1 = gson1.toJson(products);
+        BufferedWriter writer1 = new BufferedWriter(new OutputStreamWriter(fos1));
+        writer1.write(data1);
+        writer1.flush();
+        fos1.close();
+    }
     
 }
 
--- src/ui/Manager.java
+++ src/ui/Manager.java
@@ -1,9 +1,83 @@
 package ui;
 
+import model.Category;
+import model.Controller;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Manager {
+    private Scanner sc;
+    private Controller controller;
+    public Manager() {
+        sc = new Scanner(System.in);
+        controller = new Controller();
+    }
 
     public static void main(String[] args) {
-        
+        Manager m = new Manager();
+        m.init();
+    }
+
+    public void init(){
+        try {
+            controller.loadOrder();
+            controller.loadProduct();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void menu(){
+        System.out.println("Bienvenido a Mercadolibre Beta\n" +
+                "Que podemos hacer por ti?\n" +
+                "\t1. Registrar un producto\n" +
+                "\t2. Incrementar inventario de un producto\n" +
+                "\t3. Hacer un pedido\n" +
+                "\t4. Buscar productos\n" +
+                "\t5. Buscar pedidos\n" +
+                "\t6. Salir");
+        switch (sc.nextInt()){
+            case 1:
+
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            default:
+                System.out.println("Por favor ingresa una opcion valida");
+        }
+    }
+    public void registerProduct(){
+        System.out.println("Estas regitrando un producto \n Ingresa el nombre del producto");
+        String name = sc.nextLine();
+        System.out.println("Ingresa una descripcion del producto");
+        String description = sc.nextLine();
+        System.out.println("Ingresa el precio del producto");
+        double price = sc.nextDouble();
+        System.out.println("Ingresa la cantidad disponible del producto");
+        int availableAmount = sc.nextInt();
+        System.out.println("Ingresa el indice de la categoria a la cual pertenece el producto");
+        System.out.println(controller.showCategory());
+        int category = sc.nextInt();
+        System.out.println("Ingresa el numero de veces que este producto se ha vendido");
+        int purchasedTimes = sc.nextInt();
+        if(controller.registerProduct(name, description, price, availableAmount, category, purchasedTimes)){
+            System.out.println("El producto fue creado con exito");
+        }else{
+            System.out.println("Ha ocurrido un error:(");
+        }
+    }
+
+    public void registerOrder(){
+
     }
 
     
